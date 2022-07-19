e45d6f43102874d202cb4c339dd1f770e16c779b
Charles Kamiri <charleskamiri6@gmail.com>
2022-07-19 13:55:37 +0300

    added mobile support

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,7 +9,7 @@ const Works = () => {
                 <div className="gif">
                     <img src={require("../assets/images/arc-anim_1.gif")} alt="gif" />
                 </div>
-                <div>
+                <div className='works-text'>
                     We've been working on a variety of projects, from small to large, from simple to complex.
                 </div>
                 <button className='projects-btn'>Check out some them</button>
